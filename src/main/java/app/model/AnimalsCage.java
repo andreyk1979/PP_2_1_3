0c3ad2a2b320c4b6998fdf3f1c9b2c591a051553
andreyk1979 <andrey.kuimov@gmail.com>
2021-10-16 11:21:34 +0200

    step 4

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,14 +7,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
-    private final Timer timer;
-    private final Animal animal;
+    /*    private final Timer timer;
+        private final Animal animal;
 
+        @Autowired
+        public AnimalsCage(@Qualifier("dog") Animal animal, @Qualifier("timer")Timer timer) {
+            this.timer = timer;
+            this.animal = animal;*/
     @Autowired
-    public AnimalsCage(@Qualifier("dog") Animal animal, @Qualifier("timer")Timer timer) {
-        this.timer = timer;
-        this.animal = animal;
-    }
+    @Qualifier("dog")
+    private Animal animal;
+    @Autowired
+    @Qualifier("timer")
+    private Timer timer;
+
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -23,6 +30,7 @@ public class AnimalsCage {
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
+
     public Timer getTimer() {
         return timer;
     }
